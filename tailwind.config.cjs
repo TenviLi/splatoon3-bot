4d500623860ee6661307059d9e3f109c12da4215
gylidian <30214981+gylidian@users.noreply.github.com>
2022-10-28 11:43:34 +0800

    chore: init

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ['./index.html', './frontend/**/*.{vue,js}'],
+  theme: {
+    extend: {
+      colors: {
+        splatoon: {
+          blue: '#603bff',
+          purple: '#af50ff',
+          yellow: '#eaff3d',
+          green: '#6af7ce',
+          orange: '#ff9750',
+          red: '#ff505e',
+          brown: '#7f413f',
+
+          battle: {
+            regular: '#19d719',
+            ranked: '#f54910',
+            league: '#f02d7d',
+          },
+
+          salmonRun: '#ff5600',
+        },
+      },
+      fontFamily: {
+        splatoon1: 'var(--font-family-s1)',
+        splatoon2: 'var(--font-family-s2)',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('mobile', 'body.is-mobile &')
+      addVariant('ss', 'body.for-screenshots &')
+    }),
+  ],
+}
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { resolve } from 'path'
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
+
+// const redirectToDist = ['/assets/splatnet/', '/data/']
+const redirectToDist = ['/assets/splatnet/']
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueI18n({
+      include: resolve(__dirname, './src/assets/i18n/**'),
+    }),
+    {
+      // Quick hack to redirect dynamic assets to the /dist/ directory
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (redirectToDist.some((s) => req.url.startsWith(s))) {
+            req.url = '/dist' + req.url
+          }
+
+          next()
+        })
+      },
+    },
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+})
