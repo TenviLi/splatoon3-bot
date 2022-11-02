ce7b6fd98003013e7433372656c7f05b2073f6db
gylidian <30214981+gylidian@users.noreply.github.com>
2022-11-02 16:28:29 +0800

    fix: small problems

--- bot/send/cards/Generator.mjs
+++ bot/send/cards/Generator.mjs
@@ -7,6 +7,7 @@ import prefixedConsole from '../../common/prefixedConsole.mjs'
 import { getTopOfCurrentHour } from '../../common/util.mjs'
 import zhCN from '../../../src/assets/i18n/zh-CN.json' assert { type: 'json' }
 import localeCN from '../../../data/locale/zh-CN.json' assert { type: 'json' }
+import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
   legacy: false,
