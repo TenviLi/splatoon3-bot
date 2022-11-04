f02e016a2d6ed5542f6386b8626f5647c70609c2
gylidian <30214981+gylidian@users.noreply.github.com>
2022-11-04 11:17:30 +0800

    fix: regular gear card

--- bot/send/cards/RegularGearCard.mjs
+++ bot/send/cards/RegularGearCard.mjs
@@ -1,7 +1,40 @@
 import WxWorkGenerator, { $t, $d } from './Generator.mjs'
 import { useGearStore } from '../../../src/stores/gear.mjs'
-import { formatShortDurationFromNow } from '../../../src/common/time'
 import { getGearIcon } from '../../common/util.mjs'
+import { useTimeStore } from '../../../src/stores/time.mjs'
+
+function getDurationParts(value) {
+  let negative = (value < 0) ? '-' : '';
+  value = Math.abs(value);
+
+  let days = Math.floor(value / 86400);
+  value -= days * 86400;
+  let hours = Math.floor(value / 3600) % 24;
+  value -= hours * 3600;
+  let minutes = Math.floor(value / 60) % 60;
+  value -= minutes * 60;
+  let seconds = value % 60;
+
+  return { negative, days, hours, minutes, seconds };
+}
+
+ function formatShortDuration(value) {
+  let { negative, days, hours, minutes, seconds } = getDurationParts(value);
+
+  if (days)
+    return $t('time.days', { n: `${negative}${days}` }, days);
+  if (hours)
+    return $t('time.hours', { n: `${negative}${hours}` }, hours);
+  if (minutes)
+    return $t('time.minutes', { n: `${negative}${minutes}` }, minutes);
+  return $t('time.seconds', { n: `${negative}${seconds}` }, seconds);
+}
+
+ function formatShortDurationFromNow(value) {
+  let time = useTimeStore();
+
+  return formatShortDuration((Date.parse(value) - time.now) / 1000);
+}
 
 export default class RegularGearCard extends WxWorkGenerator {
   name = 'Regular Gear'
